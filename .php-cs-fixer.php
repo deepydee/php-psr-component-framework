b38bad0251d79371008463dea8c8bad20c1b1b92
Vitaliy Semenov <synthex@bk.ru>
2023-07-14 19:31:27 +0600

    init: start project

---
+++ .php-cs-fixer.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+return (new PhpCsFixer\Config())
+    ->setCacheFile(__DIR__ . '/var/php_cs_cache')
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in([
+                __DIR__ . '/public',
+                __DIR__ . '/src',
+                __DIR__ . '/tests',
+            ])
+            ->append([
+                __FILE__,
+            ])
+    )
+    ->setRules([
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        '@PHP80Migration' => true,
+        '@PHP80Migration:risky' => true,
+        '@PHPUnit84Migration:risky' => true,
+        'array_indentation' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'combine_consecutive_unsets' => true,
+        'class_attributes_separation' => ['elements' => ['method' => 'one', ]],
+        'multiline_whitespace_before_semicolons' => false,
+        'single_quote' => true,
+
+        'binary_operator_spaces' => [
+            'operators' => [
+                // '=>' => 'align',
+                // '=' => 'align'
+            ],
+        ],
+        // 'blank_line_after_opening_tag' => true,
+        // 'blank_line_before_statement' => true,
+        'braces' => [
+            'allow_single_line_closure' => true,
+        ],
+        // 'cast_spaces' => true,
+        // 'class_definition' => array('singleLine' => true),
+        'concat_space' => ['spacing' => 'one'],
+        'declare_equal_normalize' => true,
+        'function_typehint_space' => true,
+        'single_line_comment_style' => ['comment_types' => ['hash']],
+        'include' => true,
+        'lowercase_cast' => true,
+        // 'native_function_casing' => true,
+        // 'new_with_braces' => true,
+        // 'no_blank_lines_after_class_opening' => true,
+        // 'no_blank_lines_after_phpdoc' => true,
+        // 'no_blank_lines_before_namespace' => true,
+        // 'no_empty_comment' => true,
+        // 'no_empty_phpdoc' => true,
+        // 'no_empty_statement' => true,
+        'no_extra_blank_lines' => [
+            'tokens' => [
+                'curly_brace_block',
+                'extra',
+                // 'parenthesis_brace_block',
+                // 'square_brace_block',
+                'throw',
+                'use',
+            ],
+        ],
+        // 'no_leading_import_slash' => true,
+        // 'no_leading_namespace_whitespace' => true,
+        // 'no_mixed_echo_print' => array('use' => 'echo'),
+        'no_multiline_whitespace_around_double_arrow' => true,
+        // 'no_short_bool_cast' => true,
+        // 'no_singleline_whitespace_before_semicolons' => true,
+        'no_spaces_around_offset' => true,
+        // 'no_trailing_comma_in_list_call' => true,
+        // 'no_trailing_comma_in_singleline_array' => true,
+        // 'no_unneeded_control_parentheses' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['imports_order' => ['class', 'function', 'const']],
+        'no_whitespace_before_comma_in_array' => true,
+        'no_whitespace_in_blank_line' => true,
+        // 'normalize_index_brace' => true,
+        'object_operator_without_whitespace' => true,
+        'final_public_method_for_abstract_class' => true,
+        'final_class' => true,
+        'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
+        'php_unit_set_up_tear_down_visibility' => true,
+        'php_unit_fqcn_annotation' => true,
+        'php_unit_construct' => true,
+        'php_unit_internal_class' => true,
+        'no_useless_return' => true,
+        'no_useless_else' => true,
+        'no_superfluous_elseif' => true,
+        'strict_param' => true,
+        'strict_comparison' => true,
+        'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
+        'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => true],
+        // 'php_unit_fqcn_annotation' => true,
+        // 'phpdoc_align' => true,
+        // 'phpdoc_annotation_without_dot' => true,
+        // 'phpdoc_indent' => true,
+        // 'phpdoc_inline_tag' => true,
+        // 'phpdoc_no_access' => true,
+        // 'phpdoc_no_alias_tag' => true,
+        // 'phpdoc_no_empty_return' => true,
+        // 'phpdoc_no_package' => true,
+        // 'phpdoc_no_useless_inheritdoc' => true,
+        // 'phpdoc_return_self_reference' => true,
+        // 'phpdoc_scalar' => true,
+        // 'phpdoc_separation' => true,
+        // 'phpdoc_single_line_var_spacing' => true,
+        // 'phpdoc_summary' => true,
+        // 'phpdoc_to_comment' => true,
+        // 'phpdoc_trim' => true,
+        // 'phpdoc_types' => true,
+        // 'phpdoc_var_without_name' => true,
+        // 'increment_style' => true,
+        // 'return_type_declaration' => true,
+        // 'self_accessor' => true,
+        'self_static_accessor' => true,
+        // 'short_scalar_cast' => true,
+        // 'single_blank_line_before_namespace' => true,
+        'global_namespace_import' => true,
+        // 'single_class_element_per_statement' => true,
+        // 'space_after_semicolon' => true,
+        // 'standardize_not_equals' => true,
+        'ternary_operator_spaces' => true,
+        // 'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+        'trim_array_spaces' => true,
+        'static_lambda' => true,
+        'unary_operator_spaces' => true,
+        'whitespace_after_comma_in_array' => true,
+        'space_after_semicolon' => true,
+        // 'single_blank_line_at_eof' => false
+    ])
+    // ->setIndent("\t")
+    ->setLineEnding("\n")
+;
---
+++ Makefile
@@ -0,0 +1,32 @@
+init: docker-down-clear docker-pull docker-build-pull docker-up composer-install
+down: docker-down-clear
+check: lint analyze test
+
+docker-up:
+	docker-compose up -d
+
+docker-down-clear:
+	docker-compose down -v --remove-orphans
+
+docker-pull:
+	docker-compose pull
+
+docker-build-pull:
+	docker-compose build --pull
+
+app-init: composer-install
+
+composer-install:
+	docker-compose run --rm php-cli composer install
+
+test:
+	docker-compose run --rm php-cli composer test
+
+lint:
+	docker-compose run --rm php-cli composer php-cs-fixer fix -- --dry-run --diff
+
+cs-fix:
+	docker-compose run --rm php-cli composer php-cs-fixer fix
+
+analyze:
+	docker-compose run --rm php-cli composer psalm -- --no-diff
